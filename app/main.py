ceb8334feca16b2761f03eb9507f6f0d2d59dff2
Strus01 <maciek.struski@gmail.com>
2022-03-24 18:12:34 +0100

    comments

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import classifier as clf
 st.title('Machine Learning App')
 
 st.write('''
-#### Manually chose hyperparameters and see how it affects your model
+#### Manually choose hyperparameters and see how it affects your model
 ''')
 
 data_name = st.sidebar.selectbox('Select data', ('Customer churn', 'Titanic', 'Diabetes'))
